d21d4d5a5da516155fb1b43b490ad40f5766b117
snowingximen <snowingximen@163.com>
2018-03-10 00:34:19 +0800

    Init version

--- app/src/main/java/com/xcrane/examp/myapplication/MainActivity.java
+++ app/src/main/java/com/xcrane/examp/myapplication/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //do it
+        //change it
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
