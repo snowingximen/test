f59bd81ced69d27a81f51234b19cde7023f2a88e
snowingximen <snowingximen@163.com>
2018-03-10 00:29:44 +0800

    Init version

--- app/src/main/java/com/xcrane/examp/myapplication/MainActivity.java
+++ app/src/main/java/com/xcrane/examp/myapplication/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //do it
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
